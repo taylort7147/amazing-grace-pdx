c85cf3186e090c3fe59a1e3836d127d2f923fc32
Turnidge, Taylor S <taylort7147@gmail.com>
2019-07-21 22:52:35 -0700

    Added code-first migrations

---
+++ Editor/Migrations/20190722055130_AddNotesTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Editor.Migrations
+{
+    public partial class AddNotesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
