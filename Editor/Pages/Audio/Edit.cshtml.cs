b378343d68fd158d806ee11f5b99ff723a84f2d5
Turnidge, Taylor S <taylort7147@gmail.com>
2019-08-12 08:13:33 -0700

    Update both sides of message relationship on audio edit

--- Editor/Pages/Audio/Edit.cshtml.cs
+++ Editor/Pages/Audio/Edit.cshtml.cs
@@ -22,6 +22,11 @@ namespace Editor.Pages_Audio
         [BindProperty]
         public Audio Audio { get; set; }
 
+        [BindProperty]
+        public int? OriginalMessageId { get; set; }
+
+        public SelectList MessageIdList { get; set; }
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
@@ -37,10 +42,12 @@ namespace Editor.Pages_Audio
                 return NotFound();
             }
 
+            OriginalMessageId = Audio.MessageId;
+
             // Only show messages that don't have a linked audio reference, or are already linked to this
             var selectableMessages = _context.Message.Where(m => m.AudioId == null || m.AudioId == Audio.Id);
 
-            ViewData["MessageId"] = new SelectList(selectableMessages, "Id", "Description");
+            MessageIdList = new SelectList(selectableMessages, "Id", "Description");
             return Page();
         }
 
@@ -51,7 +58,28 @@ namespace Editor.Pages_Audio
                 return Page();
             }
 
-            _context.Attach(Audio).State = EntityState.Modified;
+            var message = await _context.Message.FindAsync(Audio.MessageId);
+            if(message == null)
+            {
+                Console.Error.WriteLine("Unexpected null message with ID: " + Audio.MessageId);
+                return Page();
+            }
+
+            // Unlink the original message if linking to a new message
+            if(OriginalMessageId != null)
+            {
+                var originalMessage = await _context.Message.FindAsync(OriginalMessageId);
+                if(originalMessage != null &&
+                        originalMessage.AudioId != message.AudioId)
+                {
+                    originalMessage.AudioId = null;
+                    _context.Update(originalMessage);
+                }
+            }
+
+            message.AudioId = Audio.Id;
+            _context.Update(Audio);
+            _context.Update(message);
 
             try
             {
