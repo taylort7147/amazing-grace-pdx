3912d3690383eb88e856753ed0c37f54fca24a34
Turnidge, Taylor S <taylort7147@gmail.com>
2019-08-20 07:31:29 -0700

    Add sorting to messages index

--- Editor/Pages/Messages/Index.cshtml.cs
+++ Editor/Pages/Messages/Index.cshtml.cs
@@ -13,6 +13,16 @@ namespace Editor.Pages_Messages
     [AllowAnonymous]
     public class IndexModel : PageModel
     {
+        public class SortOrder
+        {
+            public const string Date = "date";
+            public const string DateDescending = "date_desc";
+            public const string Title = "title";
+            public const string TitleDescending = "title_desc";
+            public const string Description = "description";
+            public const string DescriptionDescending = "description_desc";
+        }
+
         private readonly MessageContext _context;
 
         public IndexModel(MessageContext context)
@@ -22,9 +32,37 @@ namespace Editor.Pages_Messages
 
         public IList<Message> Message { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Message = await _context.Message.ToListAsync();
+            ViewData["DateSortParam"] = (sortOrder == SortOrder.DateDescending) ? SortOrder.Date : SortOrder.DateDescending;
+            ViewData["TitleSortParam"] = (sortOrder == SortOrder.Title) ? SortOrder.TitleDescending : SortOrder.Title;
+            ViewData["DescriptionSortParam"] = (sortOrder == SortOrder.Description) ? SortOrder.DescriptionDescending : SortOrder.Description;
+            var messages = from m in _context.Message select m;
+            switch(sortOrder)
+            {
+
+            case SortOrder.Title:
+                messages = messages.OrderBy(m => m.Title);
+                break;
+            case SortOrder.TitleDescending:
+                messages = messages.OrderByDescending(m => m.Title);
+                break;
+
+            case SortOrder.Description:
+                messages = messages.OrderBy(m => m.Description);
+                break;
+            case SortOrder.DescriptionDescending:
+                messages = messages.OrderByDescending(m => m.Description);
+                break;
+            case SortOrder.Date:
+                messages = messages.OrderBy(m => m.Date);
+                break;
+            case SortOrder.DateDescending:
+            default:
+                messages = messages.OrderByDescending(m=>m.Date);
+                break;
+            }
+            Message = await messages.ToListAsync();
         }
     }
 }
