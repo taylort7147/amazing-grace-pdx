988c7e42e9e18a0afb248f37443b0de57ad699e0
Turnidge, Taylor S <taylort7147@gmail.com>
2019-08-27 21:27:01 -0700

    Enable CORS
    Enabling all origins for now, until I have time to research it more.

--- MessageManager/Startup.cs
+++ MessageManager/Startup.cs
@@ -20,129 +20,141 @@ using Newtonsoft.Json;
 
 namespace MessageManager
 {
-    public class Startup
+public class Startup {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+    readonly string CorsPolicy = "_allowSpecificOrigins";
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(options =>
         {
-            Configuration = configuration;
+            options.AddPolicy(CorsPolicy,
+                              builder =>
+            {
+                builder.AllowAnyOrigin() // TODO: Limit origins
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
+
+        services.Configure<CookiePolicyOptions>(options =>
+        {
+            // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            options.CheckConsentNeeded = context => true;
+            options.MinimumSameSitePolicy = SameSiteMode.None;
+        });
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            // Password settings.
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequiredUniqueChars = 1;
+
+            // Lockout settings.
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+
+            // User settings.
+            options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            options.User.RequireUniqueEmail = false;
+        });
+
+        services.ConfigureApplicationCookie(options =>
+        {
+            // Cookie settings
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
+
+            options.LoginPath = "/Identity/Account/Login";
+            options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            options.SlidingExpiration = true;
+        });
+
+        services.AddDbContext<MessageContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MessageDb")));
+
+        services.AddMvc(config =>
+        {
+            var policy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .Build();
+            config.Filters.Add(new AuthorizeFilter(policy));
+        })
+        .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+        .AddJsonOptions(options =>
+        {
+            options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        });
+
+        services.AddSingleton<IAuthorizationHandler,
+                              ReadWriteAuthorizationHandler>();
+        services.AddSingleton<IAuthorizationHandler,
+                              AdministratorsAuthorizationHandler>();
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(
+                Constants.ReadWritePolicy, policy => policy.RequireRole(
+                    Constants.AdministratorRole,
+                    Constants.ReadWriteRole));
+            options.AddPolicy(
+                Constants.ReadOnlyPolicy, policy => policy.RequireRole(
+                    Constants.AdministratorRole,
+                    Constants.ReadWriteRole,
+                    Constants.ReadOnlyRole));
+        });
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
 
-        public IConfiguration Configuration { get; }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseCors(CorsPolicy);
+        app.UseCookiePolicy();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        app.UseAuthentication();
+        app.UseMvc();
+
+        CreateUserRoles(serviceProvider).Wait();
+    }
+    private async Task CreateUserRoles(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        await CreateRoleIfMissingAsync(roleManager, Constants.ReadOnlyRole);
+        await CreateRoleIfMissingAsync(roleManager, Constants.ReadWriteRole);
+        await CreateRoleIfMissingAsync(roleManager, Constants.AdministratorRole);
+    }
+
+    private async Task CreateRoleIfMissingAsync(RoleManager<IdentityRole> roleManager, string role)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.Configure<IdentityOptions>(options =>
-            {
-                // Password settings.
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
-
-                // Lockout settings.
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                options.Lockout.MaxFailedAccessAttempts = 5;
-                options.Lockout.AllowedForNewUsers = true;
-
-                // User settings.
-                options.User.AllowedUserNameCharacters =
-                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                options.User.RequireUniqueEmail = false;
-            });
-
-            services.ConfigureApplicationCookie(options =>
-            {
-                // Cookie settings
-                options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
-
-                options.LoginPath = "/Identity/Account/Login";
-                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-                options.SlidingExpiration = true;
-            });
-
-            services.AddDbContext<MessageContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MessageDb")));
-
-            services.AddMvc(config =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                .RequireAuthenticatedUser()
-                .Build();
-                config.Filters.Add(new AuthorizeFilter(policy));
-            })
-            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-            .AddJsonOptions(options =>
-            {
-                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-            });
-
-            services.AddSingleton<IAuthorizationHandler,
-                                  ReadWriteAuthorizationHandler>();
-            services.AddSingleton<IAuthorizationHandler,
-                                  AdministratorsAuthorizationHandler>();
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy(
-                    Constants.ReadWritePolicy, policy => policy.RequireRole(
-                        Constants.AdministratorRole,
-                        Constants.ReadWriteRole));
-                options.AddPolicy(
-                    Constants.ReadOnlyPolicy, policy => policy.RequireRole(
-                        Constants.AdministratorRole,
-                        Constants.ReadWriteRole,
-                        Constants.ReadOnlyRole));
-            });
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseAuthentication();
-            app.UseMvc();
-
-            CreateUserRoles(serviceProvider).Wait();
-        }
-        private async Task CreateUserRoles(IServiceProvider serviceProvider)
-        {
-            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            await CreateRoleIfMissingAsync(roleManager, Constants.ReadOnlyRole);
-            await CreateRoleIfMissingAsync(roleManager, Constants.ReadWriteRole);
-            await CreateRoleIfMissingAsync(roleManager, Constants.AdministratorRole);
-        }
-
-        private async Task CreateRoleIfMissingAsync(RoleManager<IdentityRole> roleManager, string role)
-        {
-            if (!await roleManager.RoleExistsAsync(role))
-            {
-                await roleManager.CreateAsync(new IdentityRole(role));
-            }
+            await roleManager.CreateAsync(new IdentityRole(role));
         }
     }
+}
 }
