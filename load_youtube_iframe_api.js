7828518ec88bee19d86b2e91cd348ad3fda4e921
Taylor <taylort7147@gmail.com>
2020-03-31 08:21:00 -0700

    Attempt to fix YouTube iFrame loading
    When the parent page loads the YouTube iFrame API asynchronously,
    not allowing the callbacks to be set up before hand, then attempt
    to call the callback after the fact.

--- load_youtube_iframe_api.js
+++ load_youtube_iframe_api.js
@@ -1,24 +1,30 @@
-if(typeof YouTubeIframeAPIReadyCallbacks === "undefined"){
-    
+if (typeof YouTubeIframeAPILoaded === "undefined") {
+    console.log("Setting up YouTubeIframeAPI handlers");
     YouTubeIframeAPIReadyCallbacks = [];
     YouTubeIframeAPILoaded = false;
-    
-    function registerYouTubeIframeAPIReadyCallback(cb){
-        YouTubeIframeAPIReadyCallbacks.push(cb);
+
+    function registerYouTubeIframeAPIReadyCallback(cb) {
+        console.log("registerYouTubeIframeAPIReadyCallback()");
+        if (YouTubeIframeAPILoaded) {
+            cb(); // Call the API is ready
+        } else {
+            YouTubeIframeAPIReadyCallbacks.push(cb);
+        }
     }
 
-    function onYouTubeIframeAPIReady() {  
+    function onYouTubeIframeAPIReady() {
         console.log("onYouTubeIframeAPIReady()");
+        YouTubeIframeAPILoaded = true;
         YouTubeIframeAPIReadyCallbacks.forEach(callback => callback());
     }
 
-    function loadYouTubeIframeAPI(){
-        if(YouTubeIframeAPILoaded) { return; }
+    function loadYouTubeIframeAPI() {
+        if (YouTubeIframeAPILoaded) { return; }
         YouTubeIframeAPILoaded = true;
         console.log("injectYouTubeIframeAPILoadScript()");
         var tag = document.createElement('script');
         tag.src = "https://www.youtube.com/iframe_api";
-        tag.type="application/javascript";
+        tag.type = "application/javascript";
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     }
