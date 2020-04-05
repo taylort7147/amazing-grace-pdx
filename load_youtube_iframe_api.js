40a6560b095cfb9f5e961b942355b4799365c75f
Taylor <taylort7147@gmail.com>
2020-04-04 22:20:22 -0700

    Fix YouTubeIframeAPI loading

--- load_youtube_iframe_api.js
+++ load_youtube_iframe_api.js
@@ -7,21 +7,26 @@ if (typeof YouTubeIframeAPILoaded === "undefined") {
         console.log("registerYouTubeIframeAPIReadyCallback()");
         console.log(YouTubeIframeAPILoaded);
         if (YouTubeIframeAPILoaded) {
+            console.log("YouTubeIframeAPI is already loaded. Calling callback directly.");
             cb(); // Call the API is ready
         } else {
+            console.log("Adding YouTubeIframeAPIReady callback.");
             YouTubeIframeAPIReadyCallbacks.push(cb);
         }
     }
 
     function onYouTubeIframeAPIReady() {
         console.log("onYouTubeIframeAPIReady()");
+        if (YouTubeIframeAPILoaded) {
+            return;
+        }
+        console.log("Calling YouTubeIframeAPIReady callbacks");
         YouTubeIframeAPILoaded = true;
         YouTubeIframeAPIReadyCallbacks.forEach(callback => callback());
     }
 
     function loadYouTubeIframeAPI() {
         if (YouTubeIframeAPILoaded) { return; }
-        YouTubeIframeAPILoaded = true;
         console.log("injectYouTubeIframeAPILoadScript()");
         var tag = document.createElement('script');
         tag.src = "https://www.youtube.com/iframe_api";
