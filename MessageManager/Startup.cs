1f1598b330642463ed89e3049d9402553411b475
taylort7147 <taylort7147@gmail.com>
2021-05-06 19:44:15 -0700

    Set CORS policy

--- MessageManager/Startup.cs
+++ MessageManager/Startup.cs
@@ -9,6 +9,7 @@ using MessageManager.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -28,11 +29,30 @@ namespace MessageManager
         }
 
         public IConfiguration Configuration { get; }
+        readonly string CorsPolicy = "_allowSpecificOrigins";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: CorsPolicy,
+                    builder =>
+                    {
+                        builder.WithOrigins("http://www.amazinggracepdx.com",
+                                            "https://www.amazinggracepdx.com")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
             services.Configure<IdentityOptions>(options =>
             {
                 // Password settings.
@@ -113,6 +133,7 @@ namespace MessageManager
 
             app.UseRouting();
             app.UseCookiePolicy();
+            app.UseCors(CorsPolicy);
 
             app.UseAuthentication();
             app.UseAuthorization();
