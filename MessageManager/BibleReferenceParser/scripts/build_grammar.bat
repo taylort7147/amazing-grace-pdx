f0a9bdd8be10f10b3acf464e1845226ada4ee3ae
taylort7147 <taylort7147@gmail.com>
2021-05-19 08:54:24 -0700

    Add grammar generation build step

---
+++ MessageManager/BibleReferenceParser/scripts/build_grammar.bat
@@ -0,0 +1,17 @@
+setlocal 
+
+set "project_dir=%~dp0.."
+set "source_dir=%project_dir%\src"
+set "classpath=.;%project_dir%\ThirdParty\antlr4\antlr-4.9.2-complete.jar;%classpath%"
+set "file_list=%source_dir%\Grammar\*.g4"
+set "output_dir=%project_dir%\src\Grammar\Generated"
+set "flags=-Dlanguage=CSharp -package BibleReferenceParser.Grammar.Generated -listener -no-visitor"
+
+echo Project directory: %project_dir%
+echo Source directory: %source_dir%
+echo Output directory: %output_dir%
+echo Files: %file_list%
+
+java org.antlr.v4.Tool -o "%output_dir%" %flags% %file_list%
+
+endlocal
